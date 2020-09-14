bd4408c104c4a351375a3b00e904e7f28357b2df
fakhirarslan <fakhirarslan@gmail.com>
2020-09-14 16:23:47 +0500

    user profiling minor card styling update

--- src/Components/Views/Profile/userProfile.js
+++ src/Components/Views/Profile/userProfile.js
@@ -44,13 +44,13 @@ class UserProfile extends React.Component {
                                     {this.state.user.name}
                                 </Card>
                             </Col>
-                            <Divider type="vertical" />
+                            <Divider className="divider" type="vertical" />
                             <Col span={6}>
                                 <Card title={<><MailOutlined />&nbsp;<h5>Email</h5></>} bordered={false}>
                                     {this.state.user.email}
                                 </Card>
                             </Col>
-                            <Divider type="vertical" />
+                            <Divider className="divider" type="vertical" />
                             <Col span={6}>
                                 <Card title={<><PhoneOutlined />&nbsp;<h5>Contact</h5></>} bordered={false}>
                                     {this.state.user.phone}
