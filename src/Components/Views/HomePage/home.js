729d33b46f4d8a16d319774490c2614849850863
fakhirarslan <fakhirarslan@gmail.com>
2020-09-14 10:03:47 +0500

    pull request changes completed

--- src/Components/Views/HomePage/home.js
+++ src/Components/Views/HomePage/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Menu, Avatar, Button } from 'antd';
+import { Layout, Menu } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import history from '../../Utils/history';
 import { removeUserSession, getUser } from '../../Utils/common';
@@ -32,23 +32,22 @@ class Home extends React.Component {
         return (
             <Layout className="layout">
                 <Header>
-                    <div className="logo" />
-                    {/* Welcome {this.state.user.name}<br /><br /> */}
-                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
                         <Menu.Item key="1">Home</Menu.Item>
                         <Menu.Item key="2">nav 2</Menu.Item>
                         <Menu.Item key="3">nav 3</Menu.Item>
-                        <Menu.Item key="4"><Button danger onClick={this.handleLogout}>Logout</Button></Menu.Item>
-                        <Menu.Item key="5">
-                            <Avatar size={40} icon={<UserOutlined />} />
-                        </Menu.Item>
+                        <Menu.SubMenu className="avatar" icon={<UserOutlined />}>
+                            <Menu.Item key="4">{this.state.user.name}</Menu.Item>
+                            <Menu.Item key="5">
+                                <p onClick={this.handleLogout}>Logout</p>
+                            </Menu.Item>
+                        </Menu.SubMenu>
                     </Menu>
-
                 </Header>
-                <Content style={{ padding: '0 50px' }}>
+                <Content>
                     <div className="site-layout-content">Content</div>
                 </Content>
-                <Footer style={{ textAlign: 'center' }}>Copyrights 2020</Footer>
+                <Footer>Copyrights 2020</Footer>
             </Layout>
         );
     }
